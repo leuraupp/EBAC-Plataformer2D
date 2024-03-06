6939d87a62316788294fb7eccc609932cefd47a8
leuraupp <leuraupp@gmail.com>
2024-03-06 20:27:16 -0300

    Camera and Animation Jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     [Header("References")]
     public Rigidbody2D rig2D;
 
-    [Header("Atributes")]
+    [Header("Speed Atributes")]
     public float speed;
     public float speedRun;
     public float forceJump;
     public float friction;
 
+    [Header("Animation Atributes")]
+    public float jumpScaleY = 1.1f;
+    public float jumpScaleX = 0.7f;
+    public float groundedScaleY = 0.8f;
+    public float groundedScaleX = 1.2f;
+    public float animationDuration = 0.3f;
+    public Ease ease = Ease.OutBack;
+
     private float _currentSpeed;
+    private bool _groundedAnimating = false;
 
     private void Update() {
         HandleJumping();
@@ -43,6 +53,27 @@ public class Player : MonoBehaviour
     private void HandleJumping() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             rig2D.velocity = Vector2.up * forceJump;
+            rig2D.transform.localScale = Vector2.one;
+
+            DOTween.Kill(rig2D.transform);
+
+            HandleScaleJump();
+            _groundedAnimating = false;
         }
+        if (rig2D.velocity.y == 0 && !_groundedAnimating) {
+            _groundedAnimating = true;
+            DOTween.Kill(rig2D.transform);
+            HandleScaleGrounded();
+        }
+    }
+
+    private void HandleScaleJump() {
+        rig2D.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        rig2D.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
+    private void HandleScaleGrounded() {
+        rig2D.transform.DOScaleY(groundedScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        rig2D.transform.DOScaleX(groundedScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
