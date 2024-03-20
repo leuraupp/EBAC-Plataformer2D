973fbe6d2e6289d694af07cb4ab9326c9fe7e0de
leuraupp <leuraupp@gmail.com>
2024-03-19 22:43:16 -0300

    Add collactable manager and coins

---
+++ Assets/Scripts/Collactable/ItemCollactableCoin.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ItemCollactableCoin : ItemCollactableBase
+{
+    protected override void Collect() {
+        base.Collect();
+        gameObject.transform.DOScaleX(-1, .3f).SetLoops(2, LoopType.Yoyo).OnComplete(() => Destroy(gameObject));
+    }
+
+    protected override void OnCollect() {
+        base.OnCollect();
+        ItemManager.Instance.AddCoins();
+    }
+}
---
+++ Assets/Scripts/Collactable/ItemManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public int coins;
+
+    public static ItemManager Instance;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Reset() {
+        coins = 0;
+    }
+
+    public void AddCoins(int amount = 1) {
+        coins += amount;
+    }
+}
