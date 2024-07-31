a576696bb159d07cbbf5bf203c3009631f274fcc
leuraupp <leuraupp@gmail.com>
2024-07-31 18:22:33 -0300

    Commitando particulas

--- Assets/Scripts/Collactable/ItemCollactableBase.cs
+++ Assets/Scripts/Collactable/ItemCollactableBase.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class ItemCollactableBase : MonoBehaviour
 {
     public string compareTag = "Player";
+    public ParticleSystem particleSystem;
+
+    private void Awake() {
+        if (particleSystem != null) {
+            particleSystem.transform.SetParent(null);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.transform.CompareTag(compareTag)) {
@@ -17,6 +24,8 @@ public class ItemCollactableBase : MonoBehaviour
     }
 
     protected virtual void OnCollect() {
-
+        if (particleSystem != null) {
+            particleSystem.Play();
+        }
     }
 }
